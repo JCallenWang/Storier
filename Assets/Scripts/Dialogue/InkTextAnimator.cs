d8cb14005bc45133ba58cee58aa7a7b8e307da52
g90703033 <g90703033@gmail.com>
2024-10-05 03:58:13 +0800

    Add Ink plugin and it is well-integrated will textAnimator

---
+++ Assets/Scripts/Dialogue/InkTextAnimator.cs
@@ -0,0 +1,60 @@
+using Febucci.UI;
+using Ink.Runtime;
+using TMPro;
+using UnityEngine;
+
+public class InkTextAnimator : MonoBehaviour
+{
+    public TextAsset inkJSONAsset;  // The Ink story JSON
+    private Story story;  // Ink story object
+    public TextMeshProUGUI dialogueText;  // TextMeshProUGUI for animated text
+    private TextAnimator_TMP textAnimator;  // Text Animator component
+
+    void Start()
+    {
+        story = new Story(inkJSONAsset.text);
+        textAnimator = GetComponent<TextAnimator_TMP>();
+        DisplayNextLine();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { DisplayNextLine(); }
+        if (Input.GetKeyDown(KeyCode.Keypad0)) { ChooseOption(0); }
+        if (Input.GetKeyDown(KeyCode.Keypad1)) { ChooseOption(1); }
+        if (Input.GetKeyDown(KeyCode.Keypad2)) { ChooseOption(2); }
+        if (Input.GetKeyDown(KeyCode.Keypad3)) { ChooseOption(3); }
+        if (Input.GetKeyDown(KeyCode.Keypad4)) { ChooseOption(4); }
+        if (Input.GetKeyDown(KeyCode.Keypad5)) { ChooseOption(5); }
+    }
+
+    void DisplayNextLine()
+    {
+        Debug.Log("DisplayNextLine");
+
+        if (story.canContinue)
+        {
+            string text = story.Continue();  // Get next line from Ink
+            Debug.Log("text=" + text);
+            textAnimator.SetTextToSource(text);  // Animate the text using Text Animator
+        }
+    }
+
+    // Call this method when player selects a choice
+    public bool ChooseOption(int choiceIndex)
+    {
+        try
+        {
+            story.ChooseChoiceIndex(choiceIndex);
+            DisplayNextLine();
+
+            return true;
+        }
+        catch(System.Exception e)
+        {
+
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Story/dialogue_0001.ink
@@ -0,0 +1,7 @@
+Once upon a time...
+
+ * There were two choices.
+ * There were <wave>four lines of content</wave>.
+
+- They lived happily ever after.
+    -> END
